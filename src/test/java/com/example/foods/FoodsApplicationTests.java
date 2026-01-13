94b62e71b204b6001cec81fa5f467c651a2dffd6
hoahv-2307 <hoang.van.hoa@sun-asterisk.com>
2026-01-13 16:02:33 +0700

    Fix CI: Table not found

--- src/test/java/com/example/foods/FoodsApplicationTests.java
+++ src/test/java/com/example/foods/FoodsApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.foods;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class FoodsApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+  }
 }
