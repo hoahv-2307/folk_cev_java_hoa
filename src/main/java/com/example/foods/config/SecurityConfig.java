914c7f67fb4e0bf08b56545902f108491b75c52f
hoahv-2307 <hoang.van.hoa@sun-asterisk.com>
2026-01-14 10:57:45 +0700

    Fix cycle dependencies

--- src/main/java/com/example/foods/config/SecurityConfig.java
+++ src/main/java/com/example/foods/config/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.example.foods.config;
 
-import com.example.foods.service.impl.OidcUserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,13 +17,8 @@ public class SecurityConfig {
   @Value("${app.csrf.enabled:true}")
   private boolean csrfEnabled;
 
-  private final OidcUserServiceImpl customOidcUserService;
+  private final OidcUserService customOidcUserService;
   private final OAuth2LoginFailureHandler oauth2LoginFailureHandler;
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
